00d10013ab90705bbecf03171b0a1539be1627d8
ZettaScript <zettascript@protonmail.com>
2016-11-10 19:09:17 +0100

    Upload database.php

---
+++ database.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Copyright (c) 2016 ZettaScript, Pascal Engélibert
+This file is part of ATTCHAR.
+
+	ATTCHAR is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	ATTCHAR is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with ATTCHAR.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+try {$bdd = new PDO('mysql:host=localhost;dbname=DATABASE_NAME;charset=utf8', 'DATABASE_USER', 'USER_PASSWORD');}
+catch(Exception $e) {die('Error : ' . $e->getMessage());}
+?>
