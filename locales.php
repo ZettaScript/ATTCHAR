4a611badfab1233728262ca5a89f888257367804
ZettaScript <zettascript@protonmail.com>
2017-06-14 22:06:03 +0200

    Browser's preferred language detection
    If $langdetect is true the script uses the browser's preferred language (if possible).

--- locales.php
+++ locales.php
@@ -1,5 +1,14 @@
 <?php
-if(isset($lang) and file_exists('locales/'.$lang.'.php')) {}
+# Autodetect browser language
+if(isset($langdetect) and $langdetect) {
+	$langd = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+	# Client can modify HTTP headers and modify the file path
+	# So we must verify $langd
+	if(preg_match('#[a-zA-Z_]{2}#i', $langd))
+		$lang = $langd;
+}
+
+if(isset($lang) and file_exists($_SERVER['DOCUMENT_ROOT'].'/attchar/locales/'.$lang.'.php')) {}
 else $lang = 'en';
 require('locales/'.$lang.'.php');
 ?>
